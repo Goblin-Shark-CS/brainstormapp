d01f5e3a1313287d6948c3ac000bbc889a92ca33
Dennis Corsi <denniscorsi@gmail.com>
2023-11-11 09:50:32 -0500

    fixed typos

--- server/Models/brainstormModels.js
+++ server/Models/brainstormModels.js
@@ -1,53 +1,55 @@
 const { Pool } = require('pg');
 
-const PG_URI = 'postgres://skptzoyf:u3kpKLsWJoZVDq8HU6irjCOOzhNDzJUz@suleiman.db.elephantsql.com/skptzoyf'
+const PG_URI =
+  'postgres://skptzoyf:u3kpKLsWJoZVDq8HU6irjCOOzhNDzJUz@suleiman.db.elephantsql.com/skptzoyf';
 
 const pool = new Pool({
-  connectionString: PG_URI
+  connectionString: PG_URI,
 });
 
-pool.connect((err, client, done)=>{
-  if(err) {
+pool.connect((err, client, done) => {
+  if (err) {
     console.error('Error connecting to the database', err);
-  } else{
-    console.log('Connected to the database')
+  } else {
+    console.log('Connected to the database');
   }
 });
 
-pool.query(`
+pool.query(
+  `
   CREATE TABLE IF NOT EXISTS rooms (
     _id BIGINT PRIMARY KEY,
     password VARCHAR(255),
-    roomname VARCHAR(255))`
-    , (err, result) => {
-      if (err) {
-        console.error('Error creating the roomm table')
-      } else {
-        console.log('Rooms table created successfully')
-      }
+    roomname VARCHAR(255))`,
+  (err, result) => {
+    if (err) {
+      console.error('Error creating the rooms table');
+    } else {
+      console.log('Rooms table created successfully');
     }
-  );
+  }
+);
 
-pool.query(`
-  CREATE TABLE IF NOT EXIST enries (
+pool.query(
+  `
+  CREATE TABLE IF NOT EXIST entries (
     _id SERIAL PRIMARY KEY,
     text VARCHAR(255) NOT NULL,
     room_id BIGINT FOREIGN KEY,
     votes BIGINT DEFAULT 0
-    user_id BIGINT FOREIGN KEY))`
-    , (err, result) => {
-      if (err) {
-        console.error('Error creating the roomm table')
-      } else {
-        console.log('Rooms table created successfully')
-      }
+    user_id BIGINT FOREIGN KEY))`,
+  (err, result) => {
+    if (err) {
+      console.error('Error creating the entries table');
+    } else {
+      console.log('Entries table created successfully');
     }
-)
-
+  }
+);
 
 module.exports = {
-  query: (text, params, callback) =>{
+  query: (text, params, callback) => {
     console.log('executed query', text);
-    return pool.query(text, params, callback)
-  }
-}
+    return pool.query(text, params, callback);
+  },
+};
