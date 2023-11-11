b24cb3c0883963e2474866c5d6fa9eadaf9b4b74
Omar Mirza <omirza@gmail.com>
2023-11-11 14:12:30 -0500

    Migrated remaining components to MUI

--- src/components/Entries.jsx
+++ src/components/Entries.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { reducer1, reducer2, reducer3 } from "../mainSlice";
 import Chatbox from "./Chatbox.jsx";
+import Box from '@mui/material/Box';
 
 function EntriesComponent() {
   const { entries } = useSelector((state) => state.main);
@@ -14,10 +15,9 @@ function EntriesComponent() {
   }
 
   return (
-    <div>
-      <button onClick={()=> dispatch(reducer1())}>Generate Entries</button>
+    <Box>
       <Chatbox />
-    </div>
+    </Box>
   )
 }
 
