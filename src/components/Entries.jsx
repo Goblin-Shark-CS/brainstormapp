cd8347666f552ade1b20b60f8669572fe6a3edfc
Andrew Sobottka <andrew.sobottka@gmail.com>
2023-11-11 15:22:40 -0500

    Refactors Entries.jsx to use map method

--- src/components/Entries.jsx
+++ src/components/Entries.jsx
@@ -9,25 +9,19 @@ function EntriesComponent(props) {
   const { entries } = useSelector((state) => state.main);
   const dispatch = useDispatch();
 
-  let entriesArray = [];
-  // for each item in entries, render an Entry component
-  for (let i in entries) { // TODO: Refactor using map
-    // Creates components for all entires in state
-    let key = i;
-    entriesArray.push(
-      <Entry 
-        key = {key}
-        entry = {key}
-        voteCount = {entries[i].voteCount}
-        userVote = {entries[i].userVote}
-        messageContent = {entries[i].message}
-      />
-    )
-  }
-
+  const newEntriesArray = entries.map(({id, voteCount, userVote, message}) => (
+    <Entry 
+      key = {id}
+      entry = {id}
+      voteCount = {voteCount}
+      userVote = {userVote}
+      messageContent = {message}
+    /> 
+  ))
+  
   return (
     <div>
-      {entriesArray}
+      {newEntriesArray}
     </div>
   )
 }
