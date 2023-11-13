3e04605ad292260bd7a555227a0436232453ccff
Michael Abrahams <miabraha@gmail.com>
2023-11-13 10:30:24 -0500

    BUGFIX: Add missing createRoom components

---
+++ src/createRoom.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Provider } from 'react-redux';
+import { store } from './store.js';
+import { createRoot } from 'react-dom/client';
+import CreateRoom from './components/index/CreateRoom.jsx';
+import './style.scss';
+
+
+// Roboto font for Material UI
+// import '@fontsource/roboto/300.css';
+// import '@fontsource/roboto/400.css';
+// import '@fontsource/roboto/500.css';
+// import '@fontsource/roboto/700.css';
+
+const root = createRoot(document.getElementById('root'));
+root.render(
+    <CreateRoom />
+);
