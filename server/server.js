9f922fd879d6579d05f3241bcc95667b85684b4c
Dennis Corsi <denniscorsi@gmail.com>
2023-11-13 10:58:53 -0500

    Built start route and createRoom middleware

--- server/server.js
+++ server/server.js
@@ -13,20 +13,17 @@ const { PRODUCTION, PORT } = require('./config.js');
 
 const { errMsg, dbg } = require('./logging.js');
 
+const roomController = require('./controllers/room/roomController.js');
+
 /**
  * API Routes
  */
 
-
-app.use('/start',
-  // Create room
-  // Set cookie
-  // Validate
-  (req, res) => {
-    dbg('Creating room: ', res.locals.roomId);
-    res.sendStatus(200)
-  })
-
+app.use('/start', roomController.createRoom, (req, res) => {
+  dbg('Creating room: ', res.locals.roomId);
+  // redirect user to newly created room
+  res.redirect(`/join/${res.locals.roomId}`);
+});
 
 app.use(
   '/join/:roomId',
@@ -34,9 +31,9 @@ app.use(
   /* Create session cookie */
   (req, res) => {
     dbg('Request to join room: ', req.params.roomId);
-    res.redirect(`/view/${req.params.roomId}`)
+    res.redirect(`/view/${req.params.roomId}`);
   }
-)
+);
 
 app.use(
   '/view/:roomId',
@@ -44,9 +41,8 @@ app.use(
   /* Send page */
   (req, res) => {
     dbg('Sending page: ', req.params.roomId);
-  })
-
-
+  }
+);
 
 /**
  * Static Pages
@@ -60,25 +56,21 @@ app.get('/', (req, res) => {
   return res.status(200).sendFile(path.join(__dirname, '../public/index.html'));
 });
 
-
 app.get('/index.html', (req, res) => {
   return res.redirect('/');
 });
-
 
 app.get('/style.css', (req, res) => {
   return res.status(200).sendFile(path.join(__dirname, '../src/style.css'));
 });
 
-
 /**
  * 404 handler
  */
 app.use('*', (req, res) => {
-  dbg("Unknown request: ", req.path);
+  dbg('Unknown request: ', req.path);
   res.status(404).send('Not Found');
 });
-
 
 /**
  * Global error handler
@@ -87,7 +79,7 @@ app.use('*', (req, res) => {
 const defaultErr = {
   log: 'Express error handler caught unknown middleware error',
   status: 500,
-  message: { err: 'An error occurred' }
+  message: { err: 'An error occurred' },
 };
 
 function errorHandler(err, req, res, next) {
@@ -97,12 +89,9 @@ function errorHandler(err, req, res, next) {
 }
 app.use(errorHandler);
 
-
-app.listen(PORT, () => { dbg(`Listening on port ${PORT}...`); });
-
-
-
-
+app.listen(PORT, () => {
+  dbg(`Listening on port ${PORT}...`);
+});
 
 const initialState = {
   session: {},
@@ -111,88 +100,90 @@ const initialState = {
       id: 0,
       text: 'The first brainstorm idea (From Server)',
       voteCount: 0,
-      userVote: null
+      userVote: null,
     },
     {
       id: 1,
       text: 'The second brainstorm idea (From Server)',
       voteCount: 0,
-      userVote: null
+      userVote: null,
     },
     {
       id: 2,
       text: 'The third brainstorm idea (From Server). Long: Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
       voteCount: 0,
-      userVote: null
-    }
-  ]
-}
-
-
+      userVote: null,
+    },
+  ],
+};
 
 // Websocket server
 
-const { WebSocketServer } = require('ws')
-const wsserver = new WebSocketServer({ port: 443 })
+const { WebSocketServer } = require('ws');
+const wsserver = new WebSocketServer({ port: 443 });
 
-wsserver.on('connection', ws => {
+wsserver.on('connection', (ws) => {
   // Try saving a cookie or session key to the ws object?
   // Initialize events for new client
-  dbg('New client connected!')
-  ws.session = {secret: "Secret Info Here"};
+  dbg('New client connected!');
+  ws.session = { secret: 'Secret Info Here' };
 
+  ws.on('close', () => dbg('Client has disconnected!'));
 
-  ws.on('close', () => dbg('Client has disconnected!'))
-
-  ws.on('message', rawMessage => {
+  ws.on('message', (rawMessage) => {
     let message;
     try {
       message = JSON.parse(rawMessage);
-    }
-    catch (err) {
-      return dbg('Could not parse message: ', rawMessage)
+    } catch (err) {
+      return dbg('Could not parse message: ', rawMessage);
     }
     // Route data
     // TODO: Error handling
     let response;
     try {
       switch (message.type) {
-        case "join":
+        case 'join':
           // NOTE: Initial handshake is done in Express. Session must exist at this point.
           // TODO: Validate session.
           // TODO: Read state from the database on initial join.
-          dbg('Client joined.')
+          dbg('Client joined.');
           // Store properties on ws.session
           ws.session.user_id = message.user_id; /* TODO: Set this */
           response = JSON.stringify({ type: 'init', state: initialState });
           ws.send(response);
           break;
-        case "entry":
+        case 'entry':
           // Push message to the database here
           // Do we read it back from the database? Yes.
           // TODO: Create entry_id in database
           // TODO: send Message objects with unique ID values.
-          const entryMessage = JSON.stringify({ type: 'entry', entry: {text: message.entry, entry_id: null }});
-          dbg(`distributing message: ${entryMessage}`)
+          const entryMessage = JSON.stringify({
+            type: 'entry',
+            entry: { text: message.entry, entry_id: null },
+          });
+          dbg(`distributing message: ${entryMessage}`);
           // Broadcast to all users
           // TODO: filter clients by room
-          wsserver.clients.forEach(client => { client.send(entryMessage); })
+          wsserver.clients.forEach((client) => {
+            client.send(entryMessage);
+          });
           break;
-        case "setUsername":
+        case 'setUsername':
           // Allow changing username
           dbg('Set username request: ', message);
           break;
         default:
           return dbg('Unknown message: ', message);
       }
+    } catch (err) {
+      console.log(
+        `ERROR: ${JSON.stringify(err)}. Unable to handle message: `,
+        message
+      );
     }
-    catch (err) {
-      console.log(`ERROR: ${JSON.stringify(err)}. Unable to handle message: `, message);
-    }
-  })
-
+  });
 
   ws.onerror = function () {
-    dbg('websocket error')
-  }
-})
+    dbg('websocket error');
+  };
+});
